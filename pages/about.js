e2427ffec860c4ca9a2ec63693db11c8e89ee99b
maxposure <sonu@maxposuremedia.com>
2021-01-02 00:13:34 +0530

    incomplete about page added

--- pages/about.js
+++ pages/about.js
@@ -6,11 +6,53 @@ function portfolio() {
   return (<>
     <Header/>
     <main className="container is-max-desktop py-6 mx-3-desktop">
-      <div className="hero is-large is-bold">
-        <div className="hero-body">
-          <p className="title is-1 has-text-centered">Will Be Updated Soon</p>
+      {/* <h2>About me</h2>
+      <p>Bla Bla</p> */}
+      <h3 className="title has-text-grey">Skills</h3>
+      <div className="columns">
+        <div className="column">
+          <div className="box is-fullheight">
+            <p class="is-size-4 mb-4">Frontend languages</p>
+            <div class="tags are-medium">
+              <span className="tag is-danger">HTML</span>  
+              <span className="tag is-info">CSS</span>  
+              <span className="tag is-warning">Javascript</span>
+            </div>
+          </div>
         </div>
+        
+        <div className="column">
+          <div className="box is-fullheight">
+            <p class="is-size-4 mb-4">CSS Techs</p>
+            <div class="tags are-medium">
+              <span class="tag is-danger">Sass</span>
+              <span class="tag is-primary">Bulma</span>
+              <span class="tag is-success">Bootstrap</span>
+              <span class="tag has-background-info-dark	has-text-white">TailwindCSS</span>
+              <span class="tag is-info">Material UI</span>
+            </div>
+          </div>
+
+        </div>
+        
+        <div className="column">
+          <div className="box is-fullheight">
+            <p class="is-size-4 mb-4">Javascript Techs</p>
+            <div class="tags are-medium">
+              <span class="tag is-link">Typescript</span>
+              <span class="tag is-info">Reactjs</span>
+              <span class="tag has-background-info-dark	has-text-white">Redux</span>
+              <span class="tag is-dark">Nextjs</span>
+              <span class="tag is-danger">Apollo</span>
+            </div>
+          </div>
+        </div>
+
       </div>
+      
+
+      
+      
     </main>
     <Footer/>
   </>)
